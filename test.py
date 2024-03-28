9c1d87cb2457b08b894d113189c233e30112898f
Tien <autien0809@gmail.com>
2024-03-28 19:37:34 +1030

    sd

---
+++ test.py
@@ -0,0 +1,13 @@
+def main():
+    n = int(input())
+    for i in range(0, n):        
+        x = int(input())
+        numbers = [int(num) for num in input().split()]
+        print(numbers)
+        
+
+
+if __name__ == "__main__":
+    main()
+
+
