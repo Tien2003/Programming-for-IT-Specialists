8bba63a1ad58aeca98511d0c4f72e483f0211788
Tien <autien0809@gmail.com>
2024-04-04 14:31:45 +1030

    submit

--- test.py
+++ test.py
@@ -1,46 +1,31 @@
-import itertools
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-def brute_force_set(li, n):
-    # INSERT YOUR CODE BELOW
-    # use itertools.combinations
-    # ~ 8 lines
-    # YOUR CODE HERE
-    a = [i for i in range(1, n+1)]
-    for r in range(1, len(li)+1):
-        for array in itertools.combinations(li, r):
-            visited =[]
-            for sub_array in array:
-                for element in sub_array:
-                    if (element in a) and (element not in visited):
-                        visited.append(element)
-            if len(visited) == n:
-                return array
-                    
+def sortedArrayToBST(nums):
+    if not nums:
+        return None
+    
+    mid = len(nums) // 2
+    root = TreeNode(nums[mid])
+    
+    root.left = sortedArrayToBST(nums[:mid])
+    root.right = sortedArrayToBST(nums[mid+1:])
+    
+    return root
 
-# =======================
-# EXAMPLE TESTING CASES
-set_list1 = [ # first test input
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9, 10],
-    [1, 3, 7],
-    [12],
-    [2, 4, 6, 8],
-    [11],
-]
+def inorderTraversal(root):
+    if not root:
+        return []
+    return inorderTraversal(root.left) + [root.val] + inorderTraversal(root.right)
 
-set_list2 = [ # second test input
-    [4, 2, 5],
-    [1, 7],
-    [2, 4, 8, 9, 10],
-    [5, 10],
-    [3, 5, 6],
-    [2, 6],
-    [1, 8, 9, 10]
-]
-            
-ss = brute_force_set(set_list1, 12)
-print(ss) # ([1, 2, 3], [4, 5, 6], [7, 8, 9, 10], [12], [11])
-print('---')
-ss = brute_force_set(set_list2, 10)
-print(ss) # ([1, 7], [2, 4, 8, 9, 10], [3, 5, 6])
+# Test cases
+nums1 = [-10, -3, 0, 5, 9]
+root1 = sortedArrayToBST(nums1)
+print(inorderTraversal(root1))  # Output: [-10, -3, 0, 5, 9]
+
+nums2 = [1, 3]
+root2 = sortedArrayToBST(nums2)
+print(inorderTraversal(root2))  # Output: [1, 3]
