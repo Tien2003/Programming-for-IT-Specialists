3e94594db35fadcce37d133c5fdb517cc96d6500
Tien <autien0809@gmail.com>
2024-05-09 00:11:41 +0930

    submit

---
+++ Q1-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int** arrayOfArrays(int n_rows, int n_cols){
+    int** arr = new int *[n_rows]   ;
+    for (int i=0; i<n_rows; i++){
+        arr[i] = new int[n_cols];
+    }
+
+    for (int i=0; i<n_rows; i++){
+        for (int j=0; j<n_cols; j++){
+            arr[i][j] = 1;
+        }
+    }
+    
+    for (int i=0; i<n_rows; i++){
+        delete [] arr[i];
+    }
+    delete []arr;
+
+    return arr;
+}
