b974163c3d240318d900758e236817149c789b06
Tien <autien0809@gmail.com>
2024-05-09 15:17:58 +0930

    submit

--- Q1-2.cpp
+++ Q1-2.cpp
@@ -12,23 +12,5 @@ int** arrayOfArrays(int n_rows, int n_cols){
             arr[i][j] = 1;
         }
     }
-
-    for (int i=0; i<n_rows; i++){
-        for (int j=0; j<n_cols; j++){
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-    for (int i=0; i<n_rows; i++){
-        delete [] arr[i];
-    }
-    delete []arr;
-
     return arr;
-}
-
-int main(){
-
-    arrayOfArrays(8,5);
-    return 0;
 }
