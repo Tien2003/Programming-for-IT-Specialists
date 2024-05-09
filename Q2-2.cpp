1c11863a700b29ac194981698c9cfc3328fe8d04
Tien <autien0809@gmail.com>
2024-05-09 15:53:47 +0930

    submit

--- Q2-2.cpp
+++ Q2-2.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int* singleBlock(int n){
-    int* arr = new int[n*n];
-    
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
-            arr[i*n + j] = i;
-        }
-    }
-    return arr;
-}
 
 void setColumn(int* matrix, int n, int c, int newValue){
     for (int i=0; i<n; i++){
