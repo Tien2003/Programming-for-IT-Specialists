d85b5cc97f2b089803487f90586e7f346323acfe
Tien <autien0809@gmail.com>
2024-05-09 16:19:22 +0930

    submot

--- Q1-1.cpp
+++ Q1-1.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
+
 using namespace std;
 
-int** identity(int n)        // Create an identity matrix using "array of array" method
-{
-    int** arr = new int*[n];
-    for (int i =0; i<n; i++){
-        arr[i] = new int[n];
-    }
+void evenPositions(int* arr, int n){
     for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
-            if (i==j){
-                arr[i][j] = 1;
-            }
-            else{
-                arr[i][j] = 0;
-            }
+        if (arr[i] % 2 == 0){
+            cout << i << endl;
         }
     }
-    return arr;
 }
---
+++ prac-9/Q1-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int** identity(int n)        // Create an identity matrix using "array of array" method
+{
+    int** arr = new int*[n];
+    for (int i =0; i<n; i++){
+        arr[i] = new int[n];
+    }
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            if (i==j){
+                arr[i][j] = 1;
+            }
+            else{
+                arr[i][j] = 0;
+            }
+        }
+    }
+    return arr;
+}
