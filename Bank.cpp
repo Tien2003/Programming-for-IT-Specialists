5e25e83eb332370e27a8c724b2a5e4f1178e9b0c
Tien <autien0809@gmail.com>
2024-05-16 17:51:34 +0930

    submit

---
+++ Bank.cpp
@@ -0,0 +1,43 @@
+#include "Bank.h"
+#include "Account.h"
+#include <iostream>
+
+Bank::Bank()
+{
+    capacity = 6;
+    numAccounts = 0;
+    accounts = new Account*[capacity];
+}
+
+Bank::Bank(int accCapacity)
+{
+    capacity = accCapacity;
+    numAccounts = 0;
+    accounts = new Account*[capacity];
+}
+
+void Bank::addAccount(Account* acc)
+{
+    if (numAccounts <= capacity)
+    {
+        accounts[numAccounts] = acc;
+        numAccounts += 1;
+    }
+}   
+
+Account** Bank::getAccounts()
+{
+    return accounts;
+}
+
+int Bank::getNumAccounts()
+{
+    return numAccounts;
+}
+
+void Bank::applyInterestAll(double interest)
+{
+    for (int i = 0; i<numAccounts; i++){
+        accounts[i] -> applyInterest(interest);
+    }
+}
---
+++ Bank.h
@@ -0,0 +1,25 @@
+#ifndef BANK_H
+#define BANK_H
+#include "Account.h"
+#include <string>
+
+class Bank
+{
+    int capacity;
+    int numAccounts;
+    Account** accounts;
+
+public:
+    // Constructors
+    Bank();
+    Bank(int accCapacity);
+
+    ~Bank();
+
+    // Methods
+    void addAccount(Account* acc);            // Add passed account to accounts array if room left
+    Account** getAccounts();                  // Return accounts
+    int getNumAccounts();
+    void applyInterestAll(double interest);   // Apply interest on all accounts}
+};
+#endif
